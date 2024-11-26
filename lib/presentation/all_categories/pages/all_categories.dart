85e71d34b1e6873f269001703749cb84bf42b2eb
HoussaineBenDhieb <131673785+houssainebendhieb@users.noreply.github.com>
2024-11-26 10:45:30 +0100

    all categories logic

--- lib/presentation/all_categories/pages/all_categories.dart
+++ lib/presentation/all_categories/pages/all_categories.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_e_commerce/common/cubit/category/category_cubit.dart';
 import 'package:flutter_e_commerce/core/configs/theme/app_colors.dart';
 
 class AllCategories extends StatelessWidget {
@@ -6,66 +8,96 @@ class AllCategories extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 50),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              width: 50,
-              height: 50,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(100),
-                  color: AppColors.secondBackground),
-              child: Center(
-                child: Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 10, vertical: 1),
-                  child: IconButton(
-                      onPressed: () {},
-                      icon: Icon(Icons.arrow_back_ios,
-                          color: Colors.grey.shade100)),
+    return BlocProvider(
+      create: (context) => CategoryCubit()..getCategories(),
+      child: BlocBuilder<CategoryCubit, CategoryState>(
+        builder: (context, state) {
+          if (state is CategorLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (state is CategorySucces) {
+            return Scaffold(
+              body: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 50),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100),
+                          color: AppColors.secondBackground),
+                      child: Center(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 10, vertical: 1),
+                          child: IconButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              icon: Icon(Icons.arrow_back_ios,
+                                  color: Colors.grey.shade100)),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    const Text("Shop by Categories",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold)),
+                    Expanded(
+                        child: ListView.builder(
+                            itemCount: state.categoryList.length,
+                            itemBuilder: (context, index) {
+                              return Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 5),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                      color: AppColors.secondBackground,
+                                      borderRadius: BorderRadius.circular(10)),
+                                  height: 75,
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 20),
+                                    child: Row(children: [
+                                      Container(
+                                        width: 50,
+                                        height: 50,
+                                        decoration: BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(100),
+                                            image: DecorationImage(
+                                                fit: BoxFit.cover,
+                                                image: NetworkImage(state
+                                                    .categoryList[index]
+                                                    .image))),
+                                      ),
+                                      const SizedBox(
+                                        width: 20,
+                                      ),
+                                      Text(state.categoryList[index].title,
+                                          style: const TextStyle(
+                                              fontSize: 15,
+                                              fontWeight: FontWeight.bold)),
+                                    ]),
+                                  ),
+                                ),
+                              );
+                            }))
+                  ],
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            const Text("Shop by Categories",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold)),
-            Expanded(
-                child: ListView.builder(
-                    itemCount: 4,
-                    itemBuilder: (context, index) {
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 5),
-                        child: Container(
-                          decoration: BoxDecoration(
-                              color: AppColors.secondBackground,
-                              borderRadius: BorderRadius.circular(10)),
-                          height: 60,
-                          child: const Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 20),
-                            child: Row(children: [
-                              Icon(Icons.person),
-                              SizedBox(
-                                width: 20,
-                              ),
-                              Text("Hoodies",
-                                  style: TextStyle(
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.bold)),
-                            ]),
-                          ),
-                        ),
-                      );
-                    }))
-          ],
-        ),
+            );
+          }
+          return const Text("nout found 404");
+        },
       ),
     );
   }
